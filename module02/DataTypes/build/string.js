77b3ae1c19f099152bf6cdcb93af2b116fffe0ac
Dharmindar <dharmindar@djvsoft.com>
2021-12-27 21:32:00 +0500

    boolean.ts updated

--- module02/DataTypes/build/string.js
+++ module02/DataTypes/build/string.js
@@ -1,13 +1,4 @@
 "use strict";
-let firstValue = "Hello world in double quotes";
-let secondValue = 'Hello world in single qoutes';
-let thirdValue = "Hello world through type inference";
-//String Literals or Template Literals
-let customerName = "Alfred";
-let message = `Welcome to our website Mr. ${customerName}`;
-console.log(message);
-let objString = new String("Hello world with Capital S");
-let secondObjString = new String(" more data");
-let l = objString.length;
-let concatValue = objString.concat(secondObjString.toString());
-console.log(concatValue);
+let s1 = "Hello world";
+let s2 = new String("Hello world");
+s2 = s1;
